3eff4abaa60b6c82baa71278cf8c5b80f57e2ee7
vallurusai636 <141821275+vallurusai636@users.noreply.github.com>
2025-07-14 22:40:36 +0530

    Update sai.tf

--- Day-1/PROJECT-ec2-instance-creation/sai.tf
+++ Day-1/PROJECT-ec2-instance-creation/sai.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-  region = "ap-south-1"
+    region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "sai demo" {
-  ami = "ami-0521bc4c70257a054"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0b129df8f317c229b"
-  key_name = "gitjenkins"
+resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
 }
